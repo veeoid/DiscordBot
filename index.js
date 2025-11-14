92c9dc7e1bf4b560239cdfcec3e850d70be6ab19
veeoid <vc7410@rit.edu>
2025-11-14 12:40:29 -0600

    commands handling

--- index.js
+++ index.js
@@ -13,5 +13,59 @@ client.once(Events.ClientReady, (readyClient) => {
 
 client.commands = new Collection();
 
-client.login(token);
+const foldersPath = path.join(__dirname, 'commands');
+console.log(foldersPath)
+const commandFolder = fs.readdirSync(foldersPath);
+console.log(commandFolder)
+
+for (const folder of commandFolder){
+    const commandsPath = path.join(foldersPath, folder)
+    console.log(commandsPath)
+    const commandFiles = fs.readdirSync(commandsPath).filter((file)=>file.endsWith('js'));
+    console.log(commandFiles)
+
+    for (const file of commandFiles){
+        const filePath = path.join(commandsPath, file)
+        console.log(filePath)
+        const command = require(filePath)
+        if ('data' in command && 'execute' in command){
+            client.commands.set(command.data.name, command);
+        }
+        else{
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand) return;
+    console.log(interaction);
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command){
+        console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+    }
+
+    try{
+        await command.execute(interaction)
+    }
+    catch (error){
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		} else {
+			await interaction.reply({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+        }
+    }
+})
+
+// client.login(token);
 
