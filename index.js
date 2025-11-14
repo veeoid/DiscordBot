8fcc84fea5e725bd72713289bc8edaa1d3cb14be
veeoid <vc7410@rit.edu>
2025-11-14 11:54:11 -0600

    Created commands

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const {Client, Events, GatewayIntentBits} = require('discord.js')
-const {token} = require('./config.json')
+const {Client, Events, GatewayIntentBits, Collection} = require('discord.js');
+const {token} = require('./config.json');
+const fs = require('fs');
+const path = require('path')
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-client.on("messageCreate", (message)=>{
-    console.log(message.content);
-})
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
+});
+
+client.commands = new Collection();
 
 client.login(token);
 
